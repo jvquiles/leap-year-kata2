1d7cd57d540d43554e2303f47b601057bcb14b88
Juanvi Quiles <juquiles@aidacanarias.com>
2023-04-03 17:04:28 +0200

    chore: Create project

---
+++ leap-year-kata2.Test/LeapYearShould.cs
@@ -0,0 +1,15 @@
+namespace leap_year_kata2.Test
+{
+    public class LeapYearShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
